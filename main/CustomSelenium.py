25b625ffaf4486f0d637d7f5e2dc506414d8f612
José Gabriel Alves <jose.alves@gria.io>
2024-08-21 21:54:32 -0300

    fixing selenium not opening on robocorp control panel

--- main/CustomSelenium.py
+++ main/CustomSelenium.py
@@ -5,8 +5,22 @@ from selenium.webdriver.remote.webelement import WebElement
 
 
 class CustomSelenium:
+
+    def set_chrome_options(self):
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument("--disable-extensions")
+        options.add_argument("--disable-gpu")
+        options.add_argument('--disable-web-security')
+        options.add_argument("--start-maximized")
+        options.add_argument('--remote-debugging-port=9222')
+        options.add_experimental_option("excludeSwitches", ["enable-logging"])
+        return options
+
     def set_webdriver(self):
-        self._driver = webdriver.Chrome()
+        self.options = self.set_chrome_options()
+        self._driver = webdriver.Chrome(options=self.options)
         self._driver.set_window_position(0, 0)
         print("WebDriver initialized successfully.")
 
