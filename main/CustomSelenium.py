d5f41462c2ad94f09777da5ae50c6639650627fc
José Gabriel Alves <jose.alves@gria.io>
2024-08-21 22:41:35 -0300

    another try.

--- main/CustomSelenium.py
+++ main/CustomSelenium.py
@@ -7,24 +7,15 @@ class CustomSelenium:
 
     def set_chrome_options(self):
         options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
-        options.add_argument('--no-sandbox')
-        options.add_argument("--disable-extensions")
-        options.add_argument("--disable-gpu")
-        options.add_argument('--disable-web-security')
-        options.add_argument("--start-maximized")
-        options.add_argument('--remote-debugging-port=9222')
+        # options.add_argument('--headless')
+        # options.add_argument('--no-sandbox')
         options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--disable-search-engine-choice-screen")
-        options.add_experimental_option("excludeSwitches", ["enable-logging"])
         return options
 
     def set_webdriver(self):
-        #self.options = self.set_chrome_options()
-        self._driver = webdriver.Chrome(options = 
-                                        webdriver.ChromeOptions().add_argument('--disable-search-engine-choice-screen')
-                                        )#options=self.options)
-        self._driver.set_window_position(0, 0)
+        options = self.set_chrome_options()
+        self._driver = webdriver.Chrome(options= options)
+        self._driver.set_window_position(400, 0)
         print("WebDriver initialized successfully.")
 
     def set_page_size(self, width: int, height: int):
